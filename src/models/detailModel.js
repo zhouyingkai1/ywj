d4248c6da0e21047024d74fda484a1a7ca57e1b0
zhouyingkai <zhouyingkai@timeface.cn>
2017-03-13 17:45:03 +0800

    hello dream

--- src/models/detailModel.js
+++ src/models/detailModel.js
@@ -1,7 +1,14 @@
 export default{
   namespace: 'detail',
   state:{
-    name:'2222'
+    artcleDetail:{
+      id:14,
+      isLike:false,
+      title:'风吹画廊人如玉',
+      url:'http://baidu.com',
+      like:10,
+      shortImg:'https://dn-mhke0kuv.qbox.me/13f969888162372b4034.png?imageView/2/w/800/h/400/q/100/format/png',  
+    }
   },
   subscriptions:{
     setup({dispatch,history}){
@@ -11,7 +18,7 @@ export default{
             type:'ywj/updateState',
             payload:{
               footerVisible:true,
-              headerVisible:false,
+              headerVisible:true,
             }
           })
         }
@@ -19,11 +26,26 @@ export default{
     }
   },
   effects:{
-
+    *handleLike({payload},{call,select,put}){
+      yield put({
+        type:'likeReducer',
+        payload:payload
+      })
+    }
   },
   reducers:{
     updateState(state,{payload}){
       return {...state,...payload}
-    }
+    },
+    likeReducer(state,{payload}){
+      const artcleDetail = state.artcleDetail
+      artcleDetail.isLike = payload.artcleDetail.isLike
+      if(payload.artcleDetail.isLike){
+        artcleDetail.like += 1
+      }else{
+        artcleDetail.like -= 1
+      }
+      return {...state,...artcleDetail}
+    },
   }
 }
--- src/routes/artcleDetail/ArtcleDetail.js
+++ src/routes/artcleDetail/ArtcleDetail.js
@@ -1,9 +1,15 @@
+/*
+文章详情页                        你今天觉得多此一举的注释，很久以后，你会感谢今天的自己
+*/
 import React,{Component,PropTypes} from 'react'
 import {connect} from 'dva';
 import styles from './style/artcleDetail.less'
 import {Login} from '../../components'
-import {Icon, Button, Message} from 'antd'
+import {routerRedux} from 'dva/router'
+import {Icon, Button, Message, notification} from 'antd'
 const ArtcleDetail = (props)=>{
+  const {artcleDetail} = props.detail
+  //打开登陆框
     const openDialog = (type)=>{
       props.dispatch({
         type:'ywj/updateState',
@@ -12,38 +18,52 @@ const ArtcleDetail = (props)=>{
         }
       })
     }
+    //点赞 
     const clickHeart = (id)=>{
       props.dispatch({
-        type:'home/handleLike',
-        payload:id
+        type:'detail/handleLike',
+        payload:{
+          artcleDetail:{
+            isLike: !artcleDetail.isLike
+          }
+        }
       })
     }
     const clickReport = (id)=>{
       props.dispatch({
-        type:'home/handleLike',
+        type:'detail/handleReport',
         payload:id
       })
     }
-    let item={
-      isLike:true
+    // 举报提醒文案
+    const openNotificationWithIcon = (type) => {
+      notification[type]({
+        message: '举报成功',
+        description: '我们会尽快处理，谢谢',
+        duration:3
+      });
+    }
+    // 退出详情页
+    const closeDetail = ()=>{
+      props.dispatch(routerRedux.push('/home'))
     }
     return(
       <div>
         <div className={styles.detail}>
-          <div className={styles.close}>
+          <div className={styles.close} onClick={()=>closeDetail()}>
             <Icon type="close-circle-o" />
           </div>
           <div className={styles.top}>
-            <div className={styles.topBg} style={{backgroundImage:'url(https://dn-mhke0kuv.qbox.me/a39eefc2ec6230c8a4b2.png?imageView/2/w/800/h/400/q/100/format/png)'}}></div>
+            <div className={styles.topBg} style={{backgroundImage:'url('+ artcleDetail.shortImg +')'}}></div>
             <div className={styles.topMain}>
-              <h2><a href='' target='_blank'>这是一个标题</a></h2>
+              <h2><a href={artcleDetail.rul} target='_blank'>{artcleDetail.title}</a></h2>
               <div className={styles.info}>
-                <a href='' target='_blank'>阅读全文</a>
-                <div className={item.isLike?styles.like+' '+styles.act:styles.like} onClick={()=>clickHeart(1)}>
+                <a href={artcleDetail.url} target='_blank'>阅读全文</a>
+                <div className={artcleDetail.isLike?styles.like+' '+styles.act:styles.like} onClick={()=>clickHeart(1)}>
                   <Icon type="heart" />
-                  <span>12</span>
+                  <span>{artcleDetail.like}</span>
                 </div>
-                <div className={styles.report} onClick={()=>clickReport(1)}>
+                <div className={styles.report} onClick={()=>openNotificationWithIcon('info')}>
                     <Icon type="eye-o" />
                     <span>举报</span>
                   </div>
